a7506dc0877f8e12d4ad6fc68f1f02005452c6e1
汪明月 <2411385839@qq.com>
2019-08-30 09:54:06 +0800

    提交测试类

---
+++ java-base/src/test/java/com/example/test1/Test1.java
@@ -0,0 +1,14 @@
+/**
+ * Project Name: java-base
+ * File Name: Test1
+ * Package Name: com.example.test1
+ * Date: 2019-8-30 9:53
+ * Copyright (c) 2019,All Rights Reserved.
+ */
+package com.example.test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个提交测试类");
+    }
+}
